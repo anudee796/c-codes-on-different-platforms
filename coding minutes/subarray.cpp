22aaa8b914043c11fe4f1ea44929ec63bba2444a
kasu naga veera venkata srirama anudeep <90023127+anudee796@users.noreply.github.com>
2021-11-18 12:33:03 +0530

    Update subarray.cpp

--- coding minutes/subarray.cpp
+++ coding minutes/subarray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void subprint(int arr[],int n){
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=i;j<n;j++){
             for(int k=i;k<=j;k++){
                 cout<<arr[k]<<",";
             }
