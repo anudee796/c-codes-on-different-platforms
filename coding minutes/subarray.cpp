5b66427c13e94edeaca752932eea6ce6f07f897e
kasu naga veera venkata srirama anudeep <90023127+anudee796@users.noreply.github.com>
2021-11-18 12:27:37 +0530

    Create subarray.cpp

---
+++ coding minutes/subarray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void subprint(int arr[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=i;k<=j;k++){
+                cout<<arr[k]<<",";
+            }
+            cout<<endl;
+        }
+    }
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    subprint(arr,n);
+}
